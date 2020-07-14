a292b9c5b80e4349044a03c1f2a222cc7162adbb
quanghieultk <quanghieu152@gmail.com>
2020-07-14 11:17:38 +0700

    update 3

--- ttdadn_gps.sql
+++ ttdadn_gps.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th7 13, 2020 lúc 08:03 AM
+-- Thời gian đã tạo: Th7 14, 2020 lúc 06:17 AM
 -- Phiên bản máy phục vụ: 10.4.10-MariaDB
 -- Phiên bản PHP: 7.3.12
 
@@ -43,7 +43,11 @@ CREATE TABLE `deviceroute` (
 
 INSERT INTO `deviceroute` (`id`, `id_device`, `latitude`, `longitude`, `decription`, `date_update`) VALUES
 (1, 2, 108, 14, 'Mô tả 1', '2020-07-13 00:12:12'),
-(2, 1, 109, 14, 'Mô tả 2', '2020-07-07 00:08:07');
+(2, 1, 109, 14, 'Mô tả 2', '2020-07-07 00:08:07'),
+(4, 6, 108, 15, NULL, '2020-07-14 17:00:00'),
+(5, 6, 108, 15, NULL, '2020-07-14 17:00:00'),
+(6, 6, 109, 14, NULL, '2020-06-30 17:00:00'),
+(7, 6, 110, 13, NULL, '2020-07-19 17:00:00');
 
 -- --------------------------------------------------------
 
@@ -70,10 +74,11 @@ CREATE TABLE `devices` (
 
 INSERT INTO `devices` (`id`, `user_id`, `name`, `brand`, `modecode`, `color`, `description_device`, `dateregister`, `licensePlate`, `created_at`) VALUES
 (1, 2, 'Xe Air Blade', 'HONDA', 'A123', 'Black', NULL, '2020-07-01 00:00:00', 'BC4851', '2020-07-12 17:00:00'),
-(2, 2, 'Xe Air Blade', 'HONDA', 'A123', 'Black', NULL, '2020-07-01 00:00:00', 'BC4851', '2020-07-12 17:00:00'),
+(2, 2, 'Xe Air Blade', 'FDFD', 'A123', 'PINK', NULL, '2020-07-01 00:00:00', 'BC4851', '2020-07-12 17:00:00'),
 (3, 1, 'Exciter', 'YAMAHA', 'AB12312', 'Red', NULL, '2020-07-02 00:00:00', 'BC852512', '2020-07-07 22:00:12'),
-(5, 3, '1', '2', '3', '5', '4', '2020-07-13 11:17:21', NULL, '2020-07-13 04:17:21'),
-(6, 3, 'ABC', 'HONDA', 'AB123123', 'Pink', 'Đây là xe AB', '2020-07-13 11:25:43', NULL, '2020-07-13 04:25:43');
+(6, 3, 'ABC', 'HONDA', 'AB123123', 'Pink', 'Đây là xe AB', '2020-07-13 11:25:43', NULL, '2020-07-13 04:25:43'),
+(7, 3, 'Exciter', 'YAMAHA', 'AB123', 'Red', NULL, NULL, NULL, NULL),
+(8, 3, 'Exciter', 'YAMAHA', 'AB123', 'Red', NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -106,6 +111,7 @@ CREATE TABLE `users` (
   `idcard` varchar(20) DEFAULT NULL,
   `license_number` varchar(20) DEFAULT NULL,
   `gender` varchar(10) DEFAULT NULL,
+  `permission` int(2) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -113,10 +119,10 @@ CREATE TABLE `users` (
 -- Đang đổ dữ liệu cho bảng `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`, `firstname`, `lastname`, `birthday`, `address`, `email`, `phonenumber`, `idcard`, `license_number`, `gender`, `created_at`) VALUES
-(1, 'abcasdfas', '4297f44b13955235245b2497399d7a93', NULL, NULL, NULL, NULL, 'quanghieu@gmail.com', NULL, NULL, NULL, NULL, NULL),
-(2, 'abc123', '4297f44b13955235245b2497399d7a93', NULL, NULL, NULL, NULL, 'quanghieu1@gmail.com', NULL, NULL, NULL, NULL, '2020-07-12 05:02:44'),
-(3, 'user1', '24c9e15e52afc47c225b757e7bee1f9d', 'aaaaaaaaaa', 'BN', '2020-07-01', 'Thu Duc - TP HCM', 'user1@gmail.com', '0123356262', '123426244', 'LC952663', NULL, '2020-07-13 03:14:38');
+INSERT INTO `users` (`id`, `username`, `password`, `firstname`, `lastname`, `birthday`, `address`, `email`, `phonenumber`, `idcard`, `license_number`, `gender`, `permission`, `created_at`) VALUES
+(1, 'user01', '4297f44b13955235245b2497399d7a93', NULL, NULL, NULL, NULL, 'quanghieu@gmail.com', NULL, NULL, NULL, NULL, 1, NULL),
+(2, 'abc123', '4297f44b13955235245b2497399d7a93', NULL, NULL, NULL, NULL, 'quanghieu1@gmail.com', NULL, NULL, NULL, NULL, 2, '2020-07-12 05:02:44'),
+(3, 'user1', '24c9e15e52afc47c225b757e7bee1f9d', 'aaaaaaaaaa', 'BN', '2020-07-01', 'Thu Duc - TP HCM', 'user1@gmail.com', '0123356262', '123426244', 'LC952663', NULL, 2, '2020-07-13 03:14:38');
 
 --
 -- Chỉ mục cho các bảng đã đổ
@@ -154,13 +160,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT cho bảng `deviceroute`
 --
 ALTER TABLE `deviceroute`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT cho bảng `devices`
 --
 ALTER TABLE `devices`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT cho bảng `gps_message`
